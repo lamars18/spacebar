e319b1b61e62991f732096005631c4c3ce56e9c7
Mathieu Nevins <mnevins831@gmail.com>
2018-10-15 16:35:38 -0400

    loginRegister

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,6 +1,10 @@
 import { combineReducers } from 'redux';
 import articleReducer from './articleReducer';
+import errorReducer from './errorReducer';
+import authReducer from './authReducer';
 
 export default combineReducers({
-    article: articleReducer
+    article: articleReducer,
+    errors: errorReducer,
+    auth: authReducer
 });
