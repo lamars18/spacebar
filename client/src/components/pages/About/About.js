5e39b574a5c22d455b1c4622a71c38727052c227
Mathieu Nevins <mnevins831@gmail.com>
2018-10-16 22:47:50 -0400

    updated navbar for non-authenticated pages

--- client/src/components/pages/About/About.js
+++ client/src/components/pages/About/About.js
@@ -1,42 +1,62 @@
-import React from 'react';
-import "./About.css";
+import React, { Component } from 'react';
+import MainNav from '../../layout/MainNav';
+import Header from '../../layout/Header';
 
-export default () => {
-  return (
-    <div id="about-page" className="container">
-      <div className="row">
-        <div className="col"></div>
-        <div className="col-12">
-          <h1 className="display-4 mt-4">About SpaceBar</h1>
-          <p className="lead">The Social Media Site for Techies</p>
-          <p>SpaceBar is a valuable networking tool for coders to connect, exchange ideas, and stay current on technology trends.  Other coder "networks" allow sharing of coding issues but do not promote networking to identify potential collaborators on projects, meetups, speaking engagement, etc.</p>
-        </div>
-        <div className="col"></div>
-      </div>
-      <div className="row mt-2">
-        <div className="col"></div>
-        <div className="col-4 text-center">
+class About extends Component {
+  state = {
+    appName: "SpaceBar",
+    orgName: "GT Project Team",
+    year: new Date().getFullYear()
+  };
 
-          <h4>Features</h4>
-          <ul id="feature-list" className="text-left fa-ul list-group list-group-flush">
-            <li className="list-group-item"><i className="fa-li fa fa-check app-color-5"></i>User Authentication</li>
-            <li className="list-group-item"><i className="fa-li fa fa-check app-color-5"></i>Customizable Profiles</li>
-            <li className="list-group-item"><i className="fa-li fa fa-check app-color-5"></i>Latest News</li>
-            <li className="list-group-item"><i className="fa-li fa fa-check app-color-5"></i>Blogging</li>
-            <li className="list-group-item"><i className="fa-li fa fa-check app-color-5"></i>Chat</li>
-            <li className="list-group-item"><i className="fa-li fa fa-check app-color-5"></i>Responsive Design</li>
-          </ul>
+  render() {
+    return (
+      <div>
+        <MainNav />
+        <Header
+          title={this.state.appName}
+          message="Discover what's out there."
+        ></Header>
+        <div id="about-page" className="container">
+          <div className="row">
+            <div className="col"></div>
+            <div className="col-12">
+              <h1 className="display-4 mt-4">About SpaceBar</h1>
+              <p className="lead">The Social Media Site for Techies</p>
+              <p>SpaceBar is a valuable networking tool for coders to connect, exchange ideas, and stay current on technology trends.  Other coder "networks" allow sharing of coding issues but do not promote networking to identify potential collaborators on projects, meetups, speaking engagement, etc.</p>
+            </div>
+            <div className="col"></div>
+          </div>
+          <div className="row mt-2">
+            <div className="col"></div>
+            <div className="col-4 text-center">
 
-          {/* <ul className="text-left">Contributors
+              <h4>Features</h4>
+              <ul id="feature-list" className="text-left fa-ul list-group list-group-flush">
+                <li className="list-group-item"><i className="fa-li fa fa-check app-color-5"></i>User Authentication</li>
+                <li className="list-group-item"><i className="fa-li fa fa-check app-color-5"></i>Customizable Profiles</li>
+                <li className="list-group-item"><i className="fa-li fa fa-check app-color-5"></i>Latest News</li>
+                <li className="list-group-item"><i className="fa-li fa fa-check app-color-5"></i>Blogging</li>
+                <li className="list-group-item"><i className="fa-li fa fa-check app-color-5"></i>Chat</li>
+                <li className="list-group-item"><i className="fa-li fa fa-check app-color-5"></i>Responsive Design</li>
+              </ul>
+
+              {/* <ul className="text-left">Contributors
             <li>Jenni - Express, Routing, Mongoose, MongoDB, React Routes/Context UI</li>
             <li>Randy - Front End Design</li>
             <li>Mathieu - Backend/User Authentication, Quality Control</li>
             <li>Shetia - Backend/Frontend, Blog, Presentation</li>
           </ul> */}
+            </div>
+            <div className="col"></div>
+          </div>
         </div>
-        <div className="col"></div>
       </div>
-    </div>
-  );
-};
+    )
+  }
+}
+
+export default About;
+
+
 
--- client/src/components/pages/Login.js
+++ client/src/components/pages/Login.js
@@ -3,6 +3,9 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { loginUser } from '../../actions/authentication';
 import classnames from 'classnames';
+import MainNav from '../layout/MainNav';
+import Header from '../layout/Header';
+
 
 class Login extends Component {
 
@@ -11,7 +14,11 @@ class Login extends Component {
         this.state = {
             email: '',
             password: '',
-            errors: {}
+            errors: {},
+            appName: "SpaceBar",
+            orgName: "GT Project Team",
+            year: new Date().getFullYear()
+
         }
         this.handleInputChange = this.handleInputChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
@@ -34,13 +41,13 @@ class Login extends Component {
 
     componentDidMount() {
         if (this.props.auth.isAuthenticated) {
-            this.props.history.push('/');
+            this.props.history.push('/home');
         }
     }
 
     componentWillReceiveProps(nextProps) {
         if (nextProps.auth.isAuthenticated) {
-            this.props.history.push('/')
+            this.props.history.push('/home')
         }
         if (nextProps.errors) {
             this.setState({
@@ -52,41 +59,49 @@ class Login extends Component {
     render() {
         const { errors } = this.state;
         return (
-            <div className="container" style={{ marginTop: '50px', width: '700px' }}>
-                <h2 style={{ marginBottom: '40px' }}>Login</h2>
-                <form onSubmit={this.handleSubmit}>
-                    <div className="form-group">
-                        <input
-                            type="email"
-                            placeholder="Email"
-                            className={classnames('form-control form-control-lg', {
-                                'is-invalid': errors.email
-                            })}
-                            name="email"
-                            onChange={this.handleInputChange}
-                            value={this.state.email}
-                        />
-                        {errors.email && (<div className="invalid-feedback">{errors.email}</div>)}
-                    </div>
-                    <div className="form-group">
-                        <input
-                            type="password"
-                            placeholder="Password"
-                            className={classnames('form-control form-control-lg', {
-                                'is-invalid': errors.password
-                            })}
-                            name="password"
-                            onChange={this.handleInputChange}
-                            value={this.state.password}
-                        />
-                        {errors.password && (<div className="invalid-feedback">{errors.password}</div>)}
-                    </div>
-                    <div className="form-group">
-                        <button type="submit" className="btn app-btn-primary">
-                            Login User
+            <div>
+                <MainNav />
+                <Header
+                    title={this.state.appName}
+                    message="Discover what's out there."
+
+                ></Header>
+                <div className="container" style={{ marginTop: '50px', width: '700px' }}>
+                    <h2 style={{ marginBottom: '40px' }}>Login</h2>
+                    <form onSubmit={this.handleSubmit}>
+                        <div className="form-group">
+                            <input
+                                type="email"
+                                placeholder="Email"
+                                className={classnames('form-control form-control-lg', {
+                                    'is-invalid': errors.email
+                                })}
+                                name="email"
+                                onChange={this.handleInputChange}
+                                value={this.state.email}
+                            />
+                            {errors.email && (<div className="invalid-feedback">{errors.email}</div>)}
+                        </div>
+                        <div className="form-group">
+                            <input
+                                type="password"
+                                placeholder="Password"
+                                className={classnames('form-control form-control-lg', {
+                                    'is-invalid': errors.password
+                                })}
+                                name="password"
+                                onChange={this.handleInputChange}
+                                value={this.state.password}
+                            />
+                            {errors.password && (<div className="invalid-feedback">{errors.password}</div>)}
+                        </div>
+                        <div className="form-group">
+                            <button type="submit" className="btn app-btn-primary">
+                                Login User
                     </button>
-                    </div>
-                </form>
+                        </div>
+                    </form>
+                </div>
             </div>
         )
     }
