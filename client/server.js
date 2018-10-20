8d9e4260f070867c7f809c4bcef000a2478d32ab
26735jmc <13514717+jmcoleman@users.noreply.github.com>
2018-10-20 09:11:22 -0400

    chatkit

--- client/server.js
+++ client/server.js
@@ -44,18 +44,20 @@ app.use(function(req, res, next) {
   const Pusher = require('pusher');
 
   console.log("*** PUSHER ***");
-  console.log("PROD_PUSHER_APP_ID: " + process.env.PROD_PUSHER_APP_ID);
-  console.log("PROD_PUSHER_KEY: " + process.env.PROD_PUSHER_KEY);
-  console.log("PROD_PUSHER_SECRET_KEY: " + process.env.PROD_PUSHER_SECRET_KEY);
-  console.log("PUSHER_URL: " + process.env.PUSHER_URL);
+  // console.log("PROD_PUSHER_APP_ID: " + process.env.PROD_PUSHER_APP_ID);
+  // console.log("PROD_PUSHER_KEY: " + process.env.PROD_PUSHER_KEY);
+  // console.log("PROD_PUSHER_SECRET_KEY: " + process.env.PROD_PUSHER_SECRET_KEY);
+  // console.log("PUSHER_URL: " + process.env.PUSHER_URL);
 
-  const pusher = new Pusher({
-    appId: process.env.PROD_PUSHER_APP_ID,
-    key: process.env.PROD_PUSHER_KEY,
-    secret: process.env.PROD_PUSHER_SECRET_KEY,
-    cluster: 'us2',
-    encrypted: true
-  });
+  const pusher = new Pusher.forURL(process.env.REACT_APP_PUSHER_URL);
+
+  // const pusher = new Pusher({
+  //   appId: process.env.PROD_PUSHER_APP_ID,
+  //   key: process.env.PROD_PUSHER_KEY,
+  //   secret: process.env.PROD_PUSHER_SECRET_KEY,
+  //   cluster: 'us2',
+  //   encrypted: true
+  // });
 
 
   pusher.trigger('my-channel', 'my-event', {
